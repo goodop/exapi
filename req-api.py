d23c40ca969776c09d54802851bb50d93e939a37
Ang <ang@imjustgood.com>
2024-07-23 15:44:23 +0700

    Update req-api.py
    add login with old token

--- req-api.py
+++ req-api.py
@@ -16,7 +16,7 @@ def getProxies():
         return random.choice(proxies).split(':')[0]
     return None
 
-def getQR():
+def getQR(): #Token V3
     url = f'{base_url}/loginqr'
     url2 = f'{base_url}/reqpin'
     url3 = f'{base_url}/reqtoken'
@@ -42,3 +42,30 @@ def getQR():
             print(response3)
     else:
         print(f"Error: {response.get('error', 'Unknown error')}")
+
+def getQRV1(): #Token V1
+    url = f'{base_url}/loginqrv1'
+    url2 = f'{base_url}/reqpinv1'
+    url3 = f'{base_url}/reqtokenv1'
+    params = {
+        'apikey': apikey,
+        'appName': 'DESKTOPMAC\t8.5.2\tMAC\t10.15.7',
+        'certificate': '',
+        'proxy': None
+    }
+    response = session.get(url, params=params).json()
+    print(response)
+    if response['status'] == 200:
+       params2 = {
+           'apikey': apikey,
+           'session': response['result']['session']
+       }
+       response2 = session.get(url2, params=params2).json()
+       print(response2)
+       if response2 and 'authToken' in response2['result']:
+           print(response2)
+       else:
+           response3 = session.get(url3, params=params2).json()
+           print(response3)
+
+
