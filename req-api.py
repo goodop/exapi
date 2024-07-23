8a432179dc1910b66f864c884f9670e3b759d0b5
Ang <ang@imjustgood.com>
2024-07-23 15:29:42 +0700

    Update req-api.py
    test get proxy

--- req-api.py
+++ req-api.py
@@ -1,1 +1,19 @@
+import requests
+import json
+import random
+
+base_url = 'https://execross.com/api/v3'
+apikey = 'forexecman'
+session = requests.Session()
+session.headers.update({'apikey': apikey, 'Content-Type': 'application/json'})
+
+
+def getProxies():
+    url = f'{base_url}/proxies'
+    response = session.get(url).json()
+    if response and response['status'] == 200:
+        proxies = response['result']['proxies']
+        return random.choice(proxies).split(':')[0]
+    return None
+
 
