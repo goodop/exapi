9776ba40a2a3256526ca6e2a4bbb7b9c63404448
goodop <askingang@gmail.com>
2024-10-13 11:58:19 +0700

    add check khodam

---
+++ usage/python/khodam.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+
+url = 'https://execross.com/api/v3/khodam'
+headers = {
+	"apikey": "forexecman"
+}
+params = {
+	"name":"Bagas"
+}
+datas = requests.get(url,headers=headers,params=params).json()
+response = json.dumps(datas,indent=4)
+print(response)
+
+'''
+{
+    "creator": "EXECROSS",
+    "ip": "36.74.109.56",
+    "result": {
+        "description": "Khodam Ikan Kocak, kekuatan air yang cepat dan manja, serupa dengan hewan air yang cerdas dan mudah beradaptasi. Ia menawarkan keberanian dan kecerdasan, mempermudah mencapai tujuan dengan cepat dan efisien. Paruhnya seperti cakrawala, mengingat makhluk astral yang bersinergi dengan air dan kekuatan intuitif. Khodam Ikan Kocak membantu mengembangkan jiwa yang fleksibel dan berani, seperti ikan yang pandai berenang dan tahan lama.",
+        "khodam": "Ikan Kocak",
+        "name": "Bagas",
+        "poster": "https://gate.execross.com/images/khodam/014a6654-6b75-4cb9-9687-7bf97826319f.jpg",
+        "title": "Khodam anda saat ini adalah Ikan Kocak"
+    },
+    "status": 200
+}
+'''
