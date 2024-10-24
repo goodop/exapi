0d2390036dc2dad33dc92eee8c13c2105ff05834
goodop <askingang@gmail.com>
2024-10-24 15:29:34 +0700

    change result pinterestdl

--- usage/python/pinterestdl.py
+++ usage/python/pinterestdl.py
@@ -14,17 +14,50 @@ response = json.dumps(datas,indent=4)
 print(response)
 
 '''
+
+#IMAGE/VIDEO RESULT
+
+{
+    "creator": "EXECROSS",
+    "ip": "36.74.51.197",
+    "result": {
+        "follower": "",
+        "media": [
+            {
+                "thumbnail": "https://i.pinimg.com/originals/d2/2f/f2/d22ff2e893e8522b2f4cd0becbb12b17.jpg",
+                "type": "video",
+                "url": "https:https://v1.pinimg.com/videos/mc/720p/c0/62/89/c0628922d4c45a591ba8c211afdc157d.mp4"
+            },
+            {
+                "type": "image/jpg",
+                "url": "https://i.pinimg.com/originals/d2/2f/f2/d22ff2e893e8522b2f4cd0becbb12b17.jpg"
+            }
+        ],
+        "profileUrl": "https://i.pinimg.com/75x75_RS/fd/39/04/fd3904e94db0d7437ddd5b9e9c3012bb.jpg",
+        "title": "Newspaper Slideshow - After Effects",
+        "username": "Talent Touch"
+    },
+    "status": 200
+}
+
+
+
+# GIF RESULT
+
 {
     "creator": "EXECROSS",
     "ip": "36.74.51.197",
     "result": {
         "follower": "4.8M followers",
-        "gif": "https://i.pinimg.com/originals/96/c8/8b/96c88b42028685d8cab8c083595333f6.gif",
-        "image": "",
+        "media": [
+            {
+                "type": "gif",
+                "url": "https://i.pinimg.com/originals/96/c8/8b/96c88b42028685d8cab8c083595333f6.gif"
+            }
+        ],
         "profileUrl": "https://i.pinimg.com/75x75_RS/74/8f/40/748f40a1dc49bba2298936edbc05fde1.jpg",
         "title": "Art Adventure Time",
-        "username": "imgur",
-        "video": ""
+        "username": "imgur"
     },
     "status": 200
 }
